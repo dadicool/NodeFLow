15d9c413634e002ce88ac5468080a5a1470fd7a4
Dali Kilani <dkilani@zynga.com>
2012-01-12 00:55:36 -0800

    Some initial cleanup to make the repo more "portable"

--- bin/server.js
+++ bin/server.js
@@ -4,4 +4,4 @@ var NodeFlowServer = require('../lib/nodeflow-server.js').NodeFlowServer;
 
 var NodeFlowServer = new NodeFlowServer();
 
-NodeFlowServer.start("10.8.3.100", "6633");
+NodeFlowServer.start("localhost", "6633");
