d70f62050624a95ca4462fb73d42cc913dd96624
Dali <dali.kilani@gmail.com>
2012-05-12 07:18:43 +0000

    fixing the port to support running in C9 or locally

--- bin/server.js
+++ bin/server.js
@@ -4,4 +4,5 @@ var NodeFlowServer = require('../lib/nodeflow-server.js').NodeFlowServer;
 
 var NodeFlowServer = new NodeFlowServer();
 
-NodeFlowServer.start("0.0.0.0", "6633");
+//NodeFlowServer.start("0.0.0.0", "6633");
+NodeFlowServer.start("0.0.0.0", process.env.C9_PORT || 6633);
